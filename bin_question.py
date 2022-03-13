9ddda802dcef4e5a5f6592603ceda5de41b29cdc
js18user <93879664+js18user@users.noreply.github.com>
2022-03-13 22:46:29 +0200

    Add files via upload

---
+++ bin_question.py
@@ -0,0 +1,10 @@
+def bin_question():
+
+    # return 'ethrub@ticker/adarub@ticker/adausd@ticker/adausdt@ticker/xrpusdt@ticker/xrpusd@ticker/btcusdc@ticker/solusdc@ticker/solusdt@ticker/eurtusdt@ticker/usdteur@ticker/btcrub@ticker/btcrubt@ticker/usdbct@ticker/usdtbtc@ticker/btceth@ticker/ethusdt@ticker/ltchkdt@ticker/ltchkd@ticker/ltccnyt@ticker/ltccny@ticker/btcltc@ticker/ltcusd@ticker/ltcusdt@ticker/btcltc@ticker/btccadt@ticker/btccad@ticker/btccgdt@ticker/btcsgd@ticker/btchkdt@ticker/btchkd@ticker/btccnyt@ticker/btccny@ticker/btceur@ticker/btceurt@ticker/btcusd@ticker/btcusdt@ticker/bnbbtc@ticker/glmrusdt@ticker/glmrbtc@ticker'
+    return('bcteur@ticker/bctrub@ticker/adarub@ticker/ltcusdt@ticker/' +
+           'btcusdc@ticker/glmrbtc@ticker/btcusdt@ticker/' +
+           'ethusdt@ticker/solusdt@ticker/xrpusdt@ticker/bnbbtc@ticker/' +
+           'solusdc@ticker/adausdt@ticker/ethrub@ticker/' +
+           'glmrusdt@ticker/eosbtc@ticker/enjeth@ticker/zecbtc@ticker/storjbtc@ticker/bnbusdt@ticker/' +
+           'xmrbtc@ticker/xnreth@ticker/nanaeth@ticker/nanabtc@ticker/adaeth@ticker/adabtc@ticker/adausdt@ticker/' +
+           'ltceth@ticker/ltcusdt@ticker/eosusdt@ticker/rtcusdt@ticker/vetbtc@ticker/vetusdt@ticker')
